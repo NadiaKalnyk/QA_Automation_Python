1f041fa943333ea6ab4fe7ab3006873643822224
NadiaKalnyk <kalnuk.2016.nadiya@gmail.com>
2023-11-12 19:39:54 +0200

    solved additional tasks (1-2)

---
+++ hw_03_aditional_tasks.py
@@ -0,0 +1,32 @@
+# Задача №1
+# Сформуйте стрінг, в якому міститься інформація про певне слово.
+# "Word [тут слово] has [тут довжина слова, отримайте з самого слова] letters",
+# наприклад "Word 'Python' has 6 letters". Для отримання слова для аналізу скористайтеся константою або функцією input().
+# string_a = "Loren ipsun dolor sit anet, consectetur adipisci elit, sed eiusnod tenpor incidunt ut labore et dolore nagna aliqua"
+# word = "Loren"
+# if word in string_a:
+#     print(f"Word {word} has {len(word)} letters")
+# else:
+#     print(f"No word {word} in the string")
+
+# Задача №2
+# Напишіть калькулятор, який запитує два числа якщо це множення, додавання, ділення, віднімання. Або одне число якщо це
+# зняття коріння або піднесення до степіня. Виводить на екран результат.
+# print("Enter '2' if you want to use operation: '*,+,-,/ '")
+# print("Enter '1' if you want to use operation: 'sqr','^'")
+# operation = input()
+# if operation == '1':
+#     number_1 = input("Enter number ")
+#     number_1 = float(number_1)
+#     print(f"Root of the number {number_1} is {round(number_1 ** 0.5, 2)}")
+#     print(f"{number_1}^2 = {round(number_1 ** 2, 2)}")
+#     print(f"{number_1}^3 = {round(number_1 ** 3, 2)}")
+# elif operation == '2':
+#     number_1 = float(input("Enter number1 = "))
+#     number_2 = float(input("Enter number2 = "))
+#     print(f"{number_1} * {number_2} = {number_1 * number_2}")
+#     print(f"{number_1} + {number_2} = {number_1 + number_2}")
+#     print(f"{number_1} / {number_2} = {round(number_1 / number_2, 2)}")
+#     print(f"{number_1} - {number_2} = {number_1 - number_2}")
+# else:
+#     print("Wrong number for operation. Please, enter '1' or '2'")
