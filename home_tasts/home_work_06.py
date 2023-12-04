81e53efc28f69d6d54d4132080a7ceb75c702e02
NadiaKalnyk <kalnuk.2016.nadiya@gmail.com>
2023-12-04 10:24:33 +0200

    added task3

--- home_tasts/home_work_06.py
+++ home_tasts/home_work_06.py
@@ -9,7 +9,7 @@
 # f_string = тут ваш код формата ф-стрінг. | повино вийти -> Я Олена, мені 21 рік
 # format_string = тут ваш код формата формат стрінг | повино вийти -> Я Олена, мені 21 рік
 #
-# def format_string(name="оLеnА", age=21):
+# def format_string(name="оLеnА", age=21): -> None
 #     print("""I'm {0}, I'm {1} years old""".format(name.capitalize(), age))
 #
 #
@@ -28,25 +28,47 @@
 # Запитайте в користувача Пін якщо він введе три рази не вірно то напишіть що карта заблокована.
 # Використовуйте цикл while.
 
-REAL_PIN = 1111
-my_pin_code = 1113
-failed_attempts_number = 2
-def comparing_pin_cods(result : bool):
-    result = my_pin_code != REAL_PIN
-    if result == False:
-        return False
-    else:
-        return True
+# REAL_PIN = 1111
+# my_pin_code = 1113
+# failed_attempts_number = 2
+# def comparing_pin_cods(result : bool):
+#     result = my_pin_code != REAL_PIN
+#     if result == False:
+#         return False
+#     else:
+#         return True
+#
+# def bankomat(message : str):
+#     if comparing_pin_cods(result=False):
+#         if failed_attempts_number != 3 and 3 - failed_attempts_number > 0:
+#             message = f"Invalid PIN. You have {3 - failed_attempts_number}  attempts left."
+#         else:
+#             message = "You have been blocked!"
+#     else:
+#         message = "Success! You are logged in"
+#     return message
+# message_result = bankomat(comparing_pin_cods(False))
+# message_result = bankomat(comparing_pin_cods(True))
+# print(message_result)
 
-def bankomat(message : str):
-    if comparing_pin_cods(result=False):
-        if failed_attempts_number != 3 and 3 - failed_attempts_number > 0:
-            message = f"Invalid PIN. You have {3 - failed_attempts_number}  attempts left."
+
+# Задача 3 (2)
+# Напишіть програму яка в користувача запитує два числа(можуть бути числа типу інт, а можуть бути в стр).
+# Потім запитує це стр чи інт і в залежності від відповіді конкатенує їх або додає і повертає результат перемножений
+# на три. Якщо після конкатенації отримали 10 то перемноживши на 3 отримаємо 30.
+
+first_number = 2
+second_number = 3
+number_type = 'STR'
+
+def select_operation(message : str):
+    if message == True:
+        if number_type.lower() == "str":
+            message = f"String concatenation ->  {first_number+second_number}"
+        elif number_type.lower() == "int":
+            message = f"Arithmetic operation ->  {(first_number+second_number)*3}"
         else:
-            message = "You have been blocked!"
-    else:
-        message = "Success! You are logged in"
+            message = f"Wrong input type. Please, select 'str' or 'int'"
     return message
-message_result = bankomat(comparing_pin_cods(False))
-message_result = bankomat(comparing_pin_cods(True))
-print(message_result)
+result = select_operation(True)
+print(result)
