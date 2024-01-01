0b2f593e27d71fed9271177ae987949f2fec3a95
NadiaKalnyk <kalnuk.2016.nadiya@gmail.com>
2024-01-01 16:48:18 +0200

    added home_work_13

---
+++ home_work_13/magic_methods.py
@@ -0,0 +1,37 @@
+# Виберіть який з наступних магічних методів і реалізуйте його в простому класі:
+# __ne__: To check if two objects are not equal.
+# __lt__: To check if one object is less than another.
+# __le__: To check if one object is less than or equal to another.
+# __gt__: To check if one object is greater than another.
+# __ge__: To check if one object is greater than or equal to another.
+# І написати свою логіку яку ви хочте.
+# Створіть два обьєта класа в якому ви це реалізували і зробіть перевірку що все працює
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def __ne__(self, other):
+        return self.name != other.name
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            return len(self.name) < len(other.name)
+        else:
+            return False
+
+    def __le__(self, other):
+        return self == other or self < other
+
+
+student_1 = Student("Ivan")
+student_2 = Student("Irina")
+print("****************** Testing metod __ne__ *************************")
+print(f"{student_1 != student_2} \n I'm not {student_2.name}. My name is {student_1.name} \n")
+
+print("****************** Testing metod __lt__ *************************")
+print(f"{student_1 < student_2} \n {len(student_1.name)} is less than {len(student_2.name)} \n")
+
+print("****************** Testing metod __le__ *************************")
+print(f"{student_1 == student_2 or student_1 < student_2} \n "
+      f"{len(student_1.name)} is less than or equal than {len(student_2.name)} \n")
