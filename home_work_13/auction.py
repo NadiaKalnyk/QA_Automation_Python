a75c3b67d2ad4ceae011f76259296483e10a4428
NadiaKalnyk <kalnuk.2016.nadiya@gmail.com>
2024-01-01 20:49:40 +0200

    method __ge__ + realization

---
+++ home_work_13/auction.py
@@ -0,0 +1,16 @@
+class Auction:
+    def __init__(self, name, lot):
+        self.lot = lot
+        self.name = name
+
+    def __ge__(self, other):
+        if isinstance(other, Auction):
+            if self.lot >= other.lot:
+                return f"{person_1.name} donat more than {person_2.name}"
+            else:
+                return f"{person_2.name} donat more than {person_1.name}"
+        raise TypeError
+
+person_1 = Auction("Ivan", 5000)
+person_2 = Auction("Luka", 800)
+print(person_1 >= person_2)
