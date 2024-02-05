8ffee04dbfb029b0934eeb2d345985f6fa2f5326
NadiaKalnyk <kalnuk.2016.nadiya@gmail.com>
2024-02-05 09:17:54 +0200

    added lesson21 (Checkboxes)

--- home_work_17_selenium/conftest.py
+++ home_work_17_selenium/conftest.py
@@ -4,26 +4,26 @@ from selenium import webdriver
 
 @pytest.fixture
 def chrome():
-    driver = webdriver.Chrome()
-    driver.get("C:/Users/admin/PycharmProjects/python_course/QA_Automation_Python/home_work_17_selenium/chromedriver")
+    driver = webdriver.Chrome(
+        executable_path="C:/Users/admin/PycharmProjects/python_course/QA_Automation_Python/home_work_17_selenium/chromedriver")
     yield driver
     driver.quit()
 
 
 @pytest.fixture(scope="class")
 def firefox(request):
-    driver = webdriver.Firefox()
-    driver.get("C:/Users/admin/PycharmProjects/python_course/QA_Automation_Python/home_work_17_selenium/geckodriver")
+    driver = webdriver.Firefox(
+        executable_path="C:/Users/admin/PycharmProjects/python_course/QA_Automation_Python/home_work_17_selenium/chromedriver/geckodriver")
     request.cls.driver = driver
-    # driver.implicitly_wait(5)
+    driver.implicitly_wait(5)  # імплісіті вейт це вся реалізація
     yield driver
     driver.quit()
 
 
 @pytest.fixture(scope="class")
 def chrome_class(request):
-    driver = webdriver.Chrome()
-    driver.get("C:/Users/admin/PycharmProjects/python_course/QA_Automation_Python/home_work_17_selenium/chromedriver")
+    driver = webdriver.Chrome(
+        executable_path="C:/Users/admin/PycharmProjects/python_course/QA_Automation_Python/home_work_17_selenium/chromedriver")
     request.cls.driver = driver
     yield driver
     driver.quit()
