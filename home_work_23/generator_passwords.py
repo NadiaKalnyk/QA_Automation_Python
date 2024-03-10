b404bb0d687f15790e4b143c66283d5fecbdf4ad
NadiaKalnyk <kalnuk.2016.nadiya@gmail.com>
2024-03-10 09:38:11 +0200

    h_w23

--- home_work_23/generator_passwords.py
+++ home_work_23/generator_passwords.py
@@ -2,10 +2,10 @@ import random
 import string
 
 
-def generate_password(length, use_numbers=True, use_lowercase=True, use_uppercase=True, use_cyrillic=True):
+def generate_password(length, use_digits=True, use_lowercase=True, use_uppercase=True, use_cyrillic=True):
     characters = ''
-    if use_numbers:
-        characters += string.numbers
+    if use_digits:
+        characters += string.digits
     if use_lowercase:
         characters += string.ascii_lowercase
     if use_uppercase:
@@ -18,13 +18,13 @@ def generate_password(length, use_numbers=True, use_lowercase=True, use_uppercas
 
 
 def main():
-    use_numbers = input("Використовувати цифри? (так/ні): ").lower() == 'так'
+    use_digits = input("Використовувати цифри? (так/ні): ").lower() == 'так'
     use_lowercase = input("Використовувати малі літери? (так/ні): ").lower() == 'так'
     use_uppercase = input("Використовувати великі літери? (так/ні): ").lower() == 'так'
     use_cyrillic = input("Використовувати кирилицю? (так/ні): ").lower() == 'так'
     length = int(input("Введіть довжину пароля: "))
 
-    password = generate_password(length, use_numbers, use_lowercase, use_uppercase, use_cyrillic)
+    password = generate_password(length, use_digits, use_lowercase, use_uppercase, use_cyrillic)
     print("Згенерований пароль:", password)
 
 
