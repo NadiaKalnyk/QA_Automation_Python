24f733860d3c81e241604f33020d7b51ea898007
NadiaKalnyk <117226153+NadiaKalnyk@users.noreply.github.com>
2024-03-11 22:14:25 +0200

    rozetkaParser.py

--- home_work_22/rozetkaParser.py
+++ home_work_22/rozetkaParser.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
 
